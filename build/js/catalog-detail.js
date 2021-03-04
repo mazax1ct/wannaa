7c89d28e071b1e6ba0cb16492cfee2db1d19d1c8
mazax1ct <vova@giveandget.ru>
2021-03-04 17:50:59 +0300

    remake detail pictures + delivery + size + refound + philosophy

--- build/js/catalog-detail.js
+++ build/js/catalog-detail.js
@@ -1,7 +1,30 @@
 $(document).ready(function() {
+  if($('body').width() > 1023) {
+    $('.js-images-slider').slick({
+      swipe: false,
+      mobileFirst: true,
+      dots: false,
+      arrows: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+
+    $('.js-thumbs').slick({
+      mobileFirst: true,
+      arrows: false,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '.js-images-slider',
+      dots: false,
+      focusOnSelect: true
+    });
+  }
+
   //слайдер луков
   if($('body').width() > 767) {
     $('.js-looks-slider').slick({
+      mobileFirst: true,
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 3,
