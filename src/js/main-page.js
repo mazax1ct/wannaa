c899a25ec5dd6ff12461fd304e52818070f7c510
mazax1ct <vova@giveandget.ru>
2021-03-10 12:04:37 +0300

    video autoplay

--- src/js/main-page.js
+++ src/js/main-page.js
@@ -5,3 +5,31 @@ $(document).ready(function() {
     dots: true
   });
 });
+
+var target = $('.elem');
+var targetPos = target.offset().top;
+var targetHeight = target.height();
+var winHeight = $(window).height();
+var scrollToElem = targetPos - winHeight;
+
+
+
+var $video = $('video');
+var $window = $(window);
+
+$window.scroll(function() {
+
+  var $topOfVideo = $video.offset().top;
+  var $bottomOfVideo = $video.offset().top + $video.outerHeight();
+
+  var $topOfScreen = $window.scrollTop();
+  var $bottomOfScreen = $window.scrollTop() + $window.innerHeight();
+
+  if(($bottomOfScreen > $bottomOfVideo) && ($topOfScreen < $topOfVideo)){
+    $video[0].play();
+    $('.video-block__video-play').hide();
+  } else {
+    $video[0].pause();
+    $('.video-block__video-play').show();
+  }
+});
